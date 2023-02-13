c32e27ace6b8e08492c7448b445f447bb9d3be22
atome5xx <tletoubgym74@gmail.com>
2023-02-13 17:55:02 +0100

    bouton connexion

--- app/src/main/java/com/example/pant/MainActivity.java
+++ app/src/main/java/com/example/pant/MainActivity.java
@@ -3,12 +3,81 @@ package com.example.pant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mLogin;
+    private EditText mMDP;
+    private Button mButton;
+
+    private Boolean mBoolean1;
+
+    private Boolean mBoolean2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLogin=findViewById(R.id.Login);
+        mMDP=findViewById(R.id.MDP);
+        mButton=findViewById(R.id.button);
+
+        mBoolean1=false;
+        mBoolean2=false;
+
+        mButton.setEnabled(false);
+
+        mLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                mBoolean1=(!s.toString().isEmpty());
+                mMDP.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        mBoolean2=(!s.toString().isEmpty());
+                        mButton.setEnabled(mBoolean1 && mBoolean2);
+                    }
+                });
+
+                mButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+            }
+        });
+
+
+
     }
 }
