752224de1ee11852d0c03a745e12ebe00d8b8145
atome5xx <tletoubgym74@gmail.com>
2023-03-02 10:08:51 +0100

    class user

---
+++ app/src/main/java/com/example/pant/modele/user.java
@@ -0,0 +1,41 @@
+package com.example.pant.modele;
+
+public class user {
+
+    private String mLogin;
+    private String mMotDePasse;
+    private String mMetier;
+
+    public String getLogin() {
+        return mLogin;
+    }
+
+    public void setLogin(String login) {
+        mLogin = login;
+    }
+
+    public String getMotDePasse() {
+        return mMotDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        mMotDePasse = motDePasse;
+    }
+
+    public String getMetier() {
+        return mMetier;
+    }
+
+    public void setMetier(String metier) {
+        mMetier = metier;
+    }
+
+
+
+    public user(String login, String motDePasse, String metier) {
+        this.mLogin = login;
+        this.mMotDePasse = motDePasse;
+        this.mMetier = metier;
+    }
+}
+
