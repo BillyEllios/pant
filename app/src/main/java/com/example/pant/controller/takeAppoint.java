f21b6873a74f9289d01583f98851c2b48263753d
atome5xx <tletoubgym74@gmail.com>
2023-03-20 18:44:00 +0100

    navigation drawer avec activity

--- app/src/main/java/com/example/pant/controller/takeAppoint.java
+++ app/src/main/java/com/example/pant/controller/takeAppoint.java
@@ -1,28 +1,19 @@
 package com.example.pant.controller;
 
-import static com.example.pant.R.id.toolbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.example.pant.R;
-import com.google.android.material.navigation.NavigationView;
 
-public class loginPage extends AppCompatActivity  {
+public class takeAppoint extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ImageView menu;
@@ -31,11 +22,11 @@ public class loginPage extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_take_appoint);
 
         drawerLayout = findViewById(R.id.drawerLayout);
         menu = findViewById(R.id.menu);
-        appointfutur=findViewById(R.id.appointfutur);
+        appointfutur = findViewById(R.id.appointfutur);
         appointpast = findViewById(R.id.appointpast);
         takeappoint = findViewById(R.id.takeappoint);
         report = findViewById(R.id.report);
@@ -50,25 +41,25 @@ public class loginPage extends AppCompatActivity  {
         appointfutur.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                recreate();
+                redirectActivity(takeAppoint.this, loginPage.class);
             }
         });
         appointpast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                redirectActivity(loginPage.this, appointPast.class);
+                redirectActivity(takeAppoint.this, appointPast.class);
             }
         });
         takeappoint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                redirectActivity(loginPage.this, takeAppoint.class);
+                recreate();
             }
         });
         report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                redirectActivity(loginPage.this, report.class);
+                redirectActivity(takeAppoint.this, report.class);
             }
         });
         logout.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/pant/modele/appoint.java
@@ -0,0 +1,45 @@
+package com.example.pant.modele;
+
+public class appoint {
+
+    String date;
+    String time;
+    String label;
+    String name;
+    String surname;
+    int id_client;
+
+    public appoint(String date, String time, String label, String name, String surname, int id_client) {
+        this.date = date;
+        this.time = time;
+        this.label = label;
+        this.name = name;
+        this.surname = surname;
+        this.id_client = id_client;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getId_client() {
+        return id_client;
+    }
+}
